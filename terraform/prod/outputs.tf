eeeeb1a48e555438e1d261daad94887736ade116
Mostafa Magdy <mostafamagdyhassan1@gmail.com>
2025-09-15 13:52:29 +0100

    Create outputs.tf

---
+++ terraform/prod/outputs.tf
@@ -0,0 +1,19 @@
+output "eks_cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
+}
+
+output "eks_cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "eks_cluster_certificate_authority" {
+  description = "EKS cluster CA"
+  value       = module.eks.cluster_certificate_authority_data
+}
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
